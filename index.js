f7aac7ccd9afcec756e4a68f8db7166a2a3c5d7a
hernancaceres <harinatha777@hotmail.com>
2023-09-09 16:48:39 -0300

    rutas posts y crear pos funcionando y nodemon instalado con éxito

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+//obtener todos los posts
+app.get("/posts",(req, res) => {
+    res.send("todos los posts")
+});
+
+//crear post
+app.get("/crear/post",(req, res) => {
+    res.send("crear post")
+});
+
 app.listen(3000, () => {
     console.log("SERVER corriendo en: http://localhost:3000");
 });
