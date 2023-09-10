05200f365b059b8a8757ad446b9e562d2d3c9cc2
hernancaceres <harinatha777@hotmail.com>
2023-09-10 09:50:18 -0300

    middlewares de páginas de error creado y funcionando

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+
+const middlewares = require('./middlewares');
+
 const app = express();
 
 //obtener todos los posts
-app.get("/posts",(req, res) => {
-    res.send("todos los posts")
+app.get("/",(req, res) => {
+    res.send("todos los artículos")
 });
 
 //crear post
-app.get("/crear/post",(req, res) => {
-    res.send("crear post")
+app.post("/",(req, res) => {
+    res.send("crear artículo")
 });
+
+//middlewares para páginas de error 404
+app.use(middlewares.notFound);
 
 app.listen(3000, () => {
     console.log("SERVER corriendo en: http://localhost:3000");
---
+++ middlewares.js
@@ -0,0 +1,10 @@
+const notFound = (req, res, next) => {
+    res.status(404);
+    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+    next(error);
+  };
+
+  module.exports = {
+    notFound,
+
+  };
