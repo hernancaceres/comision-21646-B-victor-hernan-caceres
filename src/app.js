17975e86f16b4a95c98724164363055cdcdcf992
hernancaceres <harinatha777@hotmail.com>
2023-09-14 10:03:49 -0300

    ARCHIVO APP.JS CREADO PARA ESCUCHAR EL SERVIDOR

---
+++ src/app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const path=require("node:path")
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const db = require("./db/database");
+const middlewares = require('./middlewares');
+const postsRouter = require('./routes/posts');
+
+const app = express();
+
+//INVOCAMOS AL MOTOR DE PLANTILLAS
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+//conexion y autenticacion
+
+(async () => {
+    try {
+        await db.authenticate(),
+            await db.sync();
+        console.log("CONEXION A LA BASE DE DATOS OK");
+    } catch (error) {
+        throw new Error(error)
+    }
+})()
+
+//middlewares 
+app.use(express.json()); // para recibir información
+app.use(cors());//para habilitar otras aplicaciones para realizar solicitudes
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"], // Permitir cargas desde el mismo dominio (self)
+        imgSrc: ["'self'", '*'], // Permitir imágenes desde el mismo dominio y todos los dominios
+        //imgSrc: ["'self'", 'https://empresas.blogthinkbig.com'], // Permitir imágenes desde el mismo dominio y el dominio externo
+        scriptSrc: ["'self'", 'https://cdn.jsdelivr.net'], // Permitir scripts desde el mismo dominio y el dominio de CDN
+      },
+    })
+  );//previene ataques de inyección de contenido, como ataques de scripting entre sitios (XSS).
+app.use(morgan("tiny"));//retorna el método, la ruta, el tiempo para completar la petición y código http de respuesta
+app.use('/', postsRouter);//para traer las rutas
+
+app.use(middlewares.notFound);//para manejar páginas de error 404
+app.use(middlewares.errorHandler);//para manejar errores de la aplicación 
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,7 @@
-require('dotenv').config();
+const app=require("./app")
 
-const path=require("node:path")
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const db = require("./db/database");
-const middlewares = require('./middlewares');
-const postsRouter = require('./routes/posts');
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-//INVOCAMOS AL MOTOR DE PLANTILLAS
-app.set("view engine","ejs");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-//conexion y autenticacion
-
-(async () => {
-    try {
-        await db.authenticate(),
-            await db.sync();
-        console.log("CONEXION A LA BASE DE DATOS OK");
-    } catch (error) {
-        throw new Error(error)
-    }
-})()
-
-//middlewares 
-app.use(express.json()); // para recibir información
-app.use(cors());//para habilitar otras aplicaciones para realizar solicitudes
-app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"], // Permitir cargas desde el mismo dominio (self)
-        imgSrc: ["'self'", '*'], // Permitir imágenes desde el mismo dominio y todos los dominios
-        //imgSrc: ["'self'", 'https://empresas.blogthinkbig.com'], // Permitir imágenes desde el mismo dominio y el dominio externo
-        scriptSrc: ["'self'", 'https://cdn.jsdelivr.net'], // Permitir scripts desde el mismo dominio y el dominio de CDN
-      },
-    })
-  );//previene ataques de inyección de contenido, como ataques de scripting entre sitios (XSS).
-app.use(morgan("tiny"));//retorna el método, la ruta, el tiempo para completar la petición y código http de respuesta
-app.use('/', postsRouter);//para traer las rutas
-
-app.use(middlewares.notFound);//para manejar páginas de error 404
-app.use(middlewares.errorHandler);//para manejar errores de la aplicación 
-
-const port = 3001
-app.listen(3001, () => {
-    console.log("SERVER corriendo en: http://localhost:3001");
+app.listen(PORT, () => {
+    console.log(`SERVER corriendo en: http://localhost:${PORT}`);
 });
